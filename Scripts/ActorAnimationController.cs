ba3a220cb02f58dbf314dad265ccd1f85485eaff
Philip Borgström <philip.borgstrom@gmail.com>
2021-03-08 23:42:18 +0100

    Code cleanup

--- Scripts/ActorAnimationController.cs
+++ Scripts/ActorAnimationController.cs
@@ -1,34 +1,38 @@
 using Godot;
-using System;
 
 public class ActorAnimationController : AnimatedSprite
 {
     [Signal]
     public delegate void AnimationFinished();
-    
+
+    private AnimationPlayer _animationPlayer;
+
     private Vector2 _startPostistion;
-    private AnimationPlayer _animationPlayer = null!;
+
     public override void _Ready()
     {
         _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
         _startPostistion = Position;
     }
 
-    public void PlayAnimation(AnimationState animation){
+    public void PlayAnimation(AnimationState animation)
+    {
         GD.Print(animation.ToString());
-        _animationPlayer.Play(animation.ToString());;
+        _animationPlayer.Play(animation.ToString());
     }
 
-    public void OnAnimationPlayerFinished(String animationName){
-
+    public void OnAnimationPlayerFinished(string animationName)
+    {
         Position = _startPostistion;
         EmitSignal(nameof(AnimationFinished));
     }
 }
-public enum AnimationState {
+
+public enum AnimationState
+{
     Stop,
     Right,
     Down,
     Left,
-    Up,
+    Up
 }
