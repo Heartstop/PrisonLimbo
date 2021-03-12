99aad67b958813b23709263dd39b3b2415d6dc6d
Mattias S <matte_1500@hotmail.com>
2021-03-12 17:30:41 +0100

    Add shitty entity collision move check on guard

--- Scripts/Guard.cs
+++ Scripts/Guard.cs
@@ -20,7 +20,7 @@ namespace PrisonLimbo.Scripts
             switch(_behaviourState){
                 case BehaviourState.Strolling: {
                     var randomDirection = (Direction)_random.Next(0,5);
-                    if(randomDirection == Direction.None)
+                    if(randomDirection == Direction.None && !World.CanMove(this, randomDirection))
                         return;
                     _animationController.PlayAnimation(randomDirection.ToAnimationState(), () => {
                         MapPosition = MapPosition + randomDirection.ToVector2();
