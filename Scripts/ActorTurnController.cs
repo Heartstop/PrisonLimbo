70eef76809aa6452bc2df968176688ae203eee02
Mattias S <matte_1500@hotmail.com>
2021-03-12 10:08:44 +0100

    Add a fucking gui element for time

--- Scripts/ActorTurnController.cs
+++ Scripts/ActorTurnController.cs
@@ -10,17 +10,22 @@ namespace PrisonLimbo.Scripts
         private Player _player;
         private Timer _turnTimer;
         private Timer _turnDelay;
+        private ProgressBar _timeBar;
         public override void _Ready()
         {
             _player = GetNode<Player>("../Player");
             _turnTimer = GetNode<Timer>("TurnTimer");
             _turnDelay = GetNode<Timer>("TurnDelay");
+            _timeBar = GetNode<ProgressBar>("Timebar");
 
+
+            _timeBar.MaxValue = _turnTimer.WaitTime;
             _turnTimer.Start();
         }
 
         public override void _Process(float delta){
             if(_isPlayerTurn) {
+                                _timeBar.Value = _turnTimer.TimeLeft;
                 _player.TurnProcess();
             }
         }
