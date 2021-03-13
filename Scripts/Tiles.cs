29190a6a0b54a41b43721b6cd57671712197fb34
Mattias S <matte_1500@hotmail.com>
2021-03-13 22:23:14 +0100

    Add the fucking tileset

---
+++ Scripts/Tiles.cs
@@ -0,0 +1,21 @@
+public enum Tiles {
+    Wall = 0, 
+    FloorShadow = 1,
+    Floor = 2,
+    SolidContinous = 5,
+    WallSingleStop = 6,
+    SolidSingleStop = 7,
+    WallContinous = 11,
+    WallLeftEnd = 17,
+    WallRightEnd = 25,
+    WallSingleRight = 26,
+    WallSingleTShape = 27,
+    WallSingleLeft = 28,
+    SolidSingleContinousLeft = 29, 
+    SolidSingleContinousTShape = 30,
+    SolidSingleContinousRight = 31,
+    SolidSingleLeft = 32,
+    SolidSingleTShape = 33,
+    SolidSingleRight = 34,
+
+}
--- Scripts/World.cs
+++ Scripts/World.cs
@@ -1,36 +1,53 @@
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using Godot;
-using PrisonLimbo.Scripts;
-
-public class World : TileMap
+using PrisonLimbo.Scripts.Extensions;
+using PrisonLimbo.Scripts.WorldGenerator;
+namespace PrisonLimbo.Scripts
 {
-    public override void _Ready()
+    public class World : Node
     {
-    }
+        TileMap _tileMap;
+        Random _random = new Random();
+        public override void _Ready()
+        {
+            var roomCells = new RoomMaker(_random, 4, (rand, roomW, roomH) => true).GenerateRooms(20, 20);
+            _tileMap = new Decorator(_random).Decorate(roomCells);
 
-    public bool CanMove<T>(T requestingEntity, Direction direction) where T : WorldEntity
-    {
-        var newPos = requestingEntity.MapPosition + direction.ToVector2();
-        return CanMove(requestingEntity, newPos);
-    }
+            var tileSet = (TileSet)GD.Load("res://Assets/tileset.tres");
+            _tileMap.TileSet = tileSet;
+            _tileMap.CellSize = new Vector2(16,16);
+            _tileMap.ZIndex = -100;
+            _tileMap.UpdateBitmaskRegion();
+            AddChild(_tileMap);
+        }
 
-    public bool CanMove<T>(T requestingEntity, Vector2 target) where T : WorldEntity
-    {
-        var startPos = requestingEntity.MapPosition;
-        /*
-        var cellIndex = GetCellv(target);
-        if (cellIndex != default)
-            return false;
-        */
-        return GetEntities(target)
-            .All(we => we.CanEnter(requestingEntity));
-    }
+        public bool CanMove<T>(T requestingEntity, Direction direction) where T : WorldEntity
+        {
+            var newPos = requestingEntity.MapPosition + direction.ToVector2();
+            return CanMove(requestingEntity, newPos);
+        }
 
-    public IEnumerable<WorldEntity> GetEntities(Vector2 position)
-    {
-        return GetChildren()
-            .OfType<WorldEntity>()
-            .Where(we => we.MapPosition == position);
+        public bool CanMove<T>(T requestingEntity, Vector2 target) where T : WorldEntity
+        {
+            var startPos = requestingEntity.MapPosition;
+            /*
+            var cellIndex = GetCellv(target);
+            if (cellIndex != default)
+                return false;
+            */
+            return GetEntities(target)
+                .All(we => we.CanEnter(requestingEntity));
+        }
+
+        public Vector2 MapToWorld(Vector2 mapPosistion) => mapPosistion * 16;            
+
+        public IEnumerable<WorldEntity> GetEntities(Vector2 position)
+        {
+            return GetChildren()
+                .OfType<WorldEntity>()
+                .Where(we => we.MapPosition == position);
+        }
     }
 }
