d488d61a5e753a25309b662d208ca5504688c3b2
Philip Borgström <philip.borgstrom@gmail.com>
2021-03-14 17:14:37 +0100

    Dead usings.

--- Scripts/NpcActor.cs
+++ Scripts/NpcActor.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using Godot;
 using Priority_Queue;
 using PrisonLimbo.Scripts.Extensions;
 
